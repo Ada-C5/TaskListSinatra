839ff321d5c956cd609dcb92c1d113c3bc893ed0
Leah <leahnpetersen@gmail.com>
2016-04-12 13:30:49 -0700

    created schema and database

--- lib/database.rb
+++ lib/database.rb
@@ -2,14 +2,25 @@ require "sqlite3"
 
 module TaskList
   class Database
+    attr_reader :db
 
-    def initialize(db_name)
-      @db = SQLite3::Database.new(db_name)
+    def initialize(db_name = "database")
+      @db = SQLite3::Database.new "database/#{db_name}.db"
     end
 
     def create_schema
-      # Put your ruby code here to use the @db variable
-      # to setup your schema in the database.
+    query = <<-CREATESTATEMENT
+      CREATE TABLE tasks (
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        description TEXT,
+        completed_at TEXT
+      );
+    CREATESTATEMENT
+
+    db.execute("DROP TABLE IF EXISTS tasks;")
+    db.execute(query)
     end
   end
 end
+TaskList::Database.new.create_schema
