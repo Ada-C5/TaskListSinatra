5bc1e19c54e9dae829e71df64d64b15297d7b57d
Ania Gonzalez <aniagr@gmail.com>
2016-04-12 16:44:25 -0700

    Created a form that passed task info params to index.
    Index then called a create task method.

--- lib/task_handler.rb
+++ lib/task_handler.rb
@@ -2,17 +2,20 @@ require 'sqlite3'
 require_relative 'database.rb'
 
 class TaskHandler < TaskList::Database
+attr_accessor :db
 
   def initialize(dbname = "tasks")
     @db = SQLite3::Database.new("database/#{dbname}.db")
   end
 
   def list_all
-   @db.execute("SELECT * FROM tasklist") #STILL WORKING
+   @db.execute("SELECT * FROM tasklist;") #STILL WORKING
   end
 
-  def create_task
-    @db.execute("INSERT INTO tasklist (title, description, completed_at) VALUES (params[:title], params[:description], params[:completed_at])") #STILL WORKING
+  def create_task(task_info)
+    @db.execute("INSERT INTO tasklist (title, description, completed_at)
+    VALUES (:title, :description, :completed_at);", task_info)
+     #STILL WORKING
   end
 
   def find_task #STILL WORKING
@@ -20,5 +23,3 @@ class TaskHandler < TaskList::Database
   end
 
 end
-
-mytasks = TaskHandler.new
--- tasklistapp.rb
+++ tasklistapp.rb
@@ -3,12 +3,20 @@ require_relative 'lib/task_handler.rb'
 
 class Tasklistapp < Sinatra::Base
 
+  mytasks = TaskHandler.new
+
   get '/' do
     erb :index
   end
 
-  post '/index' do
+  post '/' do
+    @task_info = params["task"]
+    mytasks.create_task(@task_info)
+    erb :index
+  end
 
+  get '/addtask' do
+    erb :addtask
   end
 
 run!
