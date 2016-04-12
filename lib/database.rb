33b0ff20bfc6417c40a4f6bbaadc969f3a686994
Jillian Boshart <Jilliliz@gmail.com>
2016-04-12 13:34:54 -0700

    Saving is important to do before you commit things. :(

--- lib/database.rb
+++ lib/database.rb
@@ -9,13 +9,16 @@ module TaskList
 
     def create_schema
       query = <<-CREATESTATEMENT
-        CREATE TABLE albums (
+        CREATE TABLE task_list (
           id INTEGER PRIMARY KEY,
           done_or_not NUMERIC,
           task TEXT NOT NULL,
           due_date TEXT,
         );
       CREATESTATEMENT
+
+      @db.execute("DROP TABLE IF EXISTS task_list;")
+      @db.execute(query)
     end
   end
 end
