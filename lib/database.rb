1f9a7f07847b422dec8d2ce0addf0630a9108eb2
Suzanne Harrison <harrison_family@hotmail.com>
2016-04-12 13:25:28 -0700

    first commit

--- lib/database.rb
+++ lib/database.rb
@@ -8,8 +8,41 @@ module TaskList
     end
 
     def create_schema
-      # Put your ruby code here to use the @db variable
-      # to setup your schema in the database.
+    puts "Creating Schema..."
+
+    query = <<-CREATESTATEMENT
+      CREATE TABLE tasks(
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        description TEXT,
+        completed_at TEXT,
+      );
+      CREATESTATEMENT
+
+      db.execute("DROP TABLE IF EXISTS albums;")
+      db.execute(query)
     end
+
+    def load!
+        puts "Preparing INSERT statements..."
+
+        insert_statement = <<-INSERTSTATEMENT
+          INSERT INTO tasks (
+            id, title, description, completed_at
+          ) VALUES (
+            :id, :title, :description, :completed_at
+          );
+        INSERTSTATEMENT
+
+        prepared_statement = db.prepare(insert_statement)
+
+        # now that we have a prepared statement...
+        # let's iterate the csv and use its values to populate our database
+        # CSV.foreach(FILE_PATH, headers: true) do |row|
+        #   prepared_statement.execute(row.to_h)
+        # end
+
+        # puts "Data import complete!"
+      end
   end
 end
