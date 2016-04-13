1c0c0a3fe92b166128fb6fc1afb9e3a406eb68e6
Alysia Brown <alysiacbrown@gmail.com>
2016-04-13 16:54:20 -0700

    primary requirements completed but site is still uggo.

--- tasklist.rb
+++ tasklist.rb
@@ -10,30 +10,27 @@ class Tasklist < Sinatra::Base
     erb :index
   end
 
+
   get '/tasks' do
     @tasks = Tasks.new
     @all_tasks = @tasks.print_data
     erb :tasks
   end
 
+
   post '/tasks' do
     @tasks = Tasks.new
 
-
     if params["add_task"]
     # create new task
-
       @new_task = params["task_hash"]
       @tasks.new_task(@new_task)
-      @all_tasks = @tasks.print_data
-
     elsif params["search"]
     # find task
       search = params["text"]
       @search_results = @tasks.find(search)
-
     end
-
+    @all_tasks = @tasks.print_data
     erb :tasks
   end
 
